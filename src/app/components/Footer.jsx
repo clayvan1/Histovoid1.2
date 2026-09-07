9cc61724b4967ab11b073248a04c7de60ce5c1d8
clayvan1 <claydrinch@gmail.com>
2026-09-07 02:13:12 -0700

    new

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -22,7 +22,7 @@ export default function Footer() {
         <p>
           This gallery was developed by{" "}
           <span className="glow">Clayvan</span>{" "}
-          from
+          with
           <span className="heart-glow"> ❤️</span>
         </p>
         <p>© {new Date().getFullYear()} Histovoid</p>
@@ -30,22 +30,6 @@ export default function Footer() {
 
       {/* Right side icons */}
       <div className="footer-right">
-        <a
-          href="https://x.com/ClayvanOmondi"
-          aria-label="Twitter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaTwitter />
-        </a>
-        <a
-          href="https://www.instagram.com/clay_dev"
-          aria-label="Instagram"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaInstagram />
-        </a>
         <a
           href="https://github.com/clayvan1"
           aria-label="Github"
